c7520a69093bd40cfd8e1bc7791881290ec41793
Ngasif1987 <134611394+Ngasif1987@users.noreply.github.com>
2024-07-04 20:47:38 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-let circleX = 400;
-let circleY = 250;
-let circleRadius = 85;
+let circleX = 200;
+let circleY = 150;
+let circleRadius = 75;
 
 let graphX = 50;
 let graphY = 300;
@@ -8,7 +8,7 @@ let graphAmplitude = 50;
 let graphPeriod = 300;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(400, 400);
   angleMode(DEGREES);
   describe(
     'Animated demonstration of a point moving around the unit circle, together with the corresponding sine and cosine values moving along their graphs.'
